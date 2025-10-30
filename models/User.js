4bca4358cd4260e3bbc327b9f0081cd9e0283801
mohamdrajab5-svg <mohamdrajab5@gmail.com>
2025-10-30 13:27:39 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
   {
+    // This is the Firebase UID (e.g., "Ua9z9lSTSaqYBWJe_Xfc-Q")
+    firebaseId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
     name: {
       type: String,
       required: true,
@@ -12,9 +17,9 @@ const userSchema = mongoose.Schema(
       required: true,
       unique: true, // No two users can have the same email
     },
-    password: {
+    bio: {
       type: String,
-      required: true,
+      default: '', // Add the bio field for the profile page
     },
   },
   {
@@ -22,23 +27,7 @@ const userSchema = mongoose.Schema(
   }
 );
 
-// This function will run *before* a new user is saved
-userSchema.pre('save', async function (next) {
-  // 'this' refers to the user being saved
-  // Only hash the password if it's new or has been modified
-  if (!this.isModified('password')) {
-    return next();
-  }
+// We no longer need the password hashing or compare functions
 
-  // 'genSalt' creates a "salt" to make the hash secure
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-// Add a method to the user schema to compare passwords
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  // 'this.password' is the hashed password stored in the database
-  return await bcrypt.compare(enteredPassword, this.password);
-};
 const User = mongoose.model('User', userSchema);
 module.exports = User;
